66594848483075cc6c8233741436ef8aeeb7ffef
Maegan Wilson <maegan@maeganwilson.com>
2020-05-02 17:55:28 -0500

    Starting project

---
+++ coffe-timer/coffe-timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffe-timer
+//
+//  Created by Maegan Wilson on 5/2/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
