c0515b40af74a26f7bdb39ee4948f1974be2f961
Maegan Wilson <maegan@maeganwilson.com>
2020-05-02 18:08:49 -0500

    Adding Brew Detail view

---
+++ coffe-timer/coffe-timer/views/brew details/BrewDetails.swift
@@ -0,0 +1,24 @@
+//
+//  BrewDetails.swift
+//  coffe-timer
+//
+//  Created by Maegan Wilson on 5/2/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct BrewDetails: View {
+  
+  var body: some View {
+    VStack {
+      Text("Hello, World!").font(.largeTitle)
+    }
+  }
+}
+
+struct BrewDetails_Previews: PreviewProvider {
+  static var previews: some View {
+    BrewDetails()
+  }
+}
