574c9d8cc73eb37008567b1bb4ae5c0bf4540e3c
Maegan Wilson <maegan@maeganwilson.com>
2020-05-25 13:18:42 -0500

    Adjusting title block

---
+++ coffee-timer/coffee-timer/views/shared/TitleBlock.swift
@@ -0,0 +1,46 @@
+//
+//  TitleBlock.swift
+//  coffee-timer
+//
+//  Created by Maegan Wilson on 5/4/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct TitleBlock: View {
+    var body: some View {
+        HStack {
+            Text("Chemex")
+                .font(.largeTitle)
+            Spacer()
+            VStack{
+                Button(action: {
+                    print("Toggle edit")
+                }){
+                    Text("Edit")
+                        .foregroundColor(.red)
+                }.padding(.bottom)
+                HStack{
+                    VStack{
+                        Text("16 g")
+                        Text("Coffee")
+                            .font(.footnote)
+                    }
+                    Text(":")
+                    VStack{
+                        Text("1 g")
+                        Text("Water")
+                            .font(.footnote)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct TitleBlock_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleBlock()
+    }
+}
