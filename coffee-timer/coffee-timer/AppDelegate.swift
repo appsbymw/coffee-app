167b4fa4540c6618d2261433f405baa2a363af20
Maegan Wilson <appsbymw@gmail.com>
2020-06-10 14:59:37 -0500

    Adding graphics

--- coffee-timer/coffee-timer/AppDelegate.swift
+++ coffee-timer/coffee-timer/AppDelegate.swift
@@ -42,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
        application to it. This property is optional since there are legitimate
        error conditions that could cause the creation of the store to fail.
       */
-      let container = NSPersistentCloudKitContainer(name: "coffee_timer")
+      let container = NSPersistentCloudKitContainer(name: "coffeeBuddy")
       container.loadPersistentStores(completionHandler: { (storeDescription, error) in
           if let error = error as NSError? {
               // Replace this implementation with code to handle the error appropriately.
